3d4ec9313031b624f4a89e08c0302941ed3e19e4
kmymy <kmymy@GTUNE_20260419>
2026-07-03 08:45:52 +0900

    一部更新

--- src/main/java/model/Mutter.java
+++ src/main/java/model/Mutter.java
@@ -3,27 +3,30 @@ import java.io.Serializable;
 
 
 public class Mutter implements Serializable {
-
+	private int id; //つぶやきID
 	private int user_Id; //ユーザーID
 	private String userName; //ユーザー名
 	private String text; //つぶやき
 
-public Mutter(int user_Id, String userName, String text) {
-		this.user_Id = user_Id;
+	
+	//つぶやき表示用のコンストラクタ
+	
+	public Mutter(int id, String userName, String text) {
+		this.id= id;
 		this.userName = userName;
 		this.text = text;
 	}
 	
-	public int getUserId() {
-		return user_Id;
+	
+	//つぶやき投稿用のコンストラクタ
+	
+public Mutter(int user_Id, String text) {
+		this.user_Id = user_Id;
+		this.text = text;
 	}
 	
-	public String getUserName() {
-		return userName;
-	}
-
-	public String getText() {
-		return text;
-	}
+	public int getUserId() {	return user_Id;	}
+	public String getUserName() {return userName;}
+	public String getText() {return text;}
 
 }
